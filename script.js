8acd93587faa758caceed64010ea29285563900d
juliacp07 <julia.cpaiva2@aluno.edu.es.gov.br>
2026-06-06 20:03:45 -0300

    Remove dark mode toggle functionality

--- script.js
+++ script.js
@@ -3,20 +3,4 @@ document.addEventListener("DOMContentLoaded", function () {
     window.alert("Bem-vindo à página dedicada ao Studio Ghibli! Explore as animações que marcaram gerações.");
     const btnTema = document.getElementById("btn-tema");
     
-    if (btnTema) {
-        btnTema.addEventListener("click", function () {
-            document.body.classList.toggle("modo-escuro");
-            
-            if (document.body.classList.contains("modo-escuro")) {
-                btnTema.textContent = "Alternar Modo Claro";
-                btnTema.classList.remove("btn-outline-light");
-                btnTema.classList.add("btn-light");
-            } else {
-                btnTema.textContent = "Alternar Modo Escuro";
-                btnTema.classList.remove("btn-light");
-                btnTema.classList.add("btn-outline-light");
-            }
-        });
-    }
-
-  
+    
